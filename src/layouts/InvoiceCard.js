88c742b3d30fd6bc45172bc4d6dd444d8ab9ea10
Alexandru Mihai Ionel <ionelalexandru01@gmail.com>
2024-09-20 22:17:31 +0300

    commit11
    -redesigned invoices

--- src/layouts/InvoiceCard.js
+++ src/layouts/InvoiceCard.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
+import { useAuth } from '../contexts/Authentification'
 
 const InvoiceCard = ({invoice}) => {
+
+    const {userAuth} = useAuth();
 
     function calculateTotalForEach(a,b) {
         const resultRounded =(a*b).toFixed(2);
@@ -14,6 +17,10 @@ const InvoiceCard = ({invoice}) => {
     let totalSum = totalEnergy+totalGas+totalWater;
     let totalSumRounded = totalSum.toFixed(2);
     let total = parseFloat(totalSumRounded);
+
+    let startBillingPeriod = invoice.billingPeriod.split(' - ')[0];
+    let endBillingPeriod = invoice.billingPeriod.split(' - ')[1];
+    let tva = (0.19*total).toFixed(2);
 
     function formateDate(dateString)
     {
@@ -57,28 +64,48 @@ const InvoiceCard = ({invoice}) => {
         return(
             <div class="card text-center m-5 custom-card mx-auto">
                 <div class="card-header mb-0">
-                    Utilities Invoice
+                    Payment Portal Utilities Invoice
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title mb-4 ">{invoice.titleInvoice}</h5>
-                    <div className="justify-content-center mb-3 mx-auto">
+                <div class="card-body mt-5 mx-5">
+
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="d-flex flex-column justify-content-start  text-start">
+                                <p className="custom-bold-text-invoice">Issued to: </p>
+                                <p className="custom-small-text">{userAuth.fullName}</p>
+                                <p className="custom-bold-text-invoice">Address: </p>
+                                <p className="custom-small-text">{userAuth.address}</p>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="d-flex flex-column justify-content-end  text-end mx-1">
+                                <p className="custom-bold-text-invoice mb-4">Invoice ID: {invoice.id}</p>
+                                <p className="custom-p-invoice-small">Start Date: {startBillingPeriod}</p>
+                                <p className="custom-p-invoice-small">End Date: {endBillingPeriod}</p>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <div className="justify-content-center mb-3 mt-4">
                         <div className="row">
                             <div className="col-md-3">
-                                <h6>Type</h6>
+                                <p className="custom-bold-text-invoice">Description</p>
                             </div>
                             <div className="col-md-3">
-                                <h6>Consumption</h6>
+                                <p className="custom-bold-text-invoice">Quantity</p>
                             </div>
                             <div className="col-md-3">
-                                <h6>Price/unit</h6>
+                                <p className="custom-bold-text-invoice">Unit Price</p>
                             </div>
                             <div className="col-md-3">
-                                <h6>Total</h6>
+                                <p className="custom-bold-text-invoice">Total</p>
                             </div>
                         </div>
         
                         <div>
-                            <p className="custom-margin-top-minus-15px">─────────────────────────────────────────</p>
+                            <p >─────────────────────────────────────────</p>
                         </div>
                         <div class="row custom-margin-top-minus-10px">
                             <div className="col-md-3">
@@ -88,7 +115,7 @@ const InvoiceCard = ({invoice}) => {
                                 <p>{invoice.waterConsumption} mc</p>
                             </div>
                             <div className="col-md-3">
-                                <p>{invoice.waterPrice} RON/mc</p>
+                                <p>{invoice.waterPrice} </p>
                             </div>
                             <div className="col-md-3">
                                 <p>{totalWater} RON</p>
@@ -103,7 +130,7 @@ const InvoiceCard = ({invoice}) => {
                                 <p>{invoice.energyConsumption} kWh</p>
                             </div>
                             <div className="col-md-3">
-                                <p>{invoice.energyPrice} RON/hWh</p>
+                                <p>{invoice.energyPrice} </p>
                             </div>
                             <div className="col-md-3">
                                 <p>{totalEnergy} RON</p>
@@ -118,20 +145,38 @@ const InvoiceCard = ({invoice}) => {
                                 <p>{invoice.gasConsumption} mc</p>
                             </div>
                             <div className="col-md-3">
-                                <p>{invoice.gasPrice} RON/mc</p>
+                                <p>{invoice.gasPrice} </p>
                             </div>
                             <div className="col-md-3">
                                 <p>{totalGas} RON</p>
                             </div>
                         </div>
-                        <p className="custom-margin-top-minus-15px">------------------------------------------------------------------------</p>
-                        <h5 className="custom-margin-top-minus-15px">Total: {total} RON </h5>
-                        <p className="custom-margin-top-minus-10px custom-p-small"> *Calculated as the total payment for all your utilities</p>
-                        <p className="custom-margin-top-minus-15px">------------------------------------------------------------------------</p>
-                        <div className="d-flex justify-content-center custom-margin-top-minus-10px">
-                            <button type="submit" className="btn btn-danger me-2 btn-lg" onClick={handlerPayment}>Report</button>
-                            <button type="submit" className="btn btn-success  btn-lg" onClick={handlerReport}>Pay now</button>
-                        </div> 
+                        <p >─────────────────────────────────────────</p>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="d-flex flex-column justify-content-start  text-start mx-3">
+                                    <p className="custom-bold-text-invoice">Subtotal:</p>
+                                    
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                            <div className="d-flex flex-column justify-content-end  text-end mx-4">
+                                    <p className="custom-bold-text-invoice">{total} RON </p>
+                                    <p className="custom-p-small mt-2">*TVA 19% |  {tva} RON</p>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        
+                     
+                        <div className="row justify-content-center mt-3">
+                            <div className="col-12 text-center mb-3">
+                                <button type="submit" className="btn custom-btn-invoice-form" onClick={handlerPayment}>Pay now</button>
+                            </div>
+                            <div className="col-12 text-center">
+                                <p className="custom-bold-text-invoice" style={{ cursor: 'pointer', marginTop:"-10px" , color:"red"}} onClick={handlerReport}>Report an issue</p>
+                            </div>
+                        </div>
                         
                     </div>
                 </div>
